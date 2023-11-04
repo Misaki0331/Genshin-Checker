f6f28e3a16918f858521b8cf7e7c6041d62a8986
水咲(みさき) <blueplanet65536@gmail.com>
2023-11-04 18:47:05 +0900

    旅人通帳のローカライズAPIの準備

---
+++ Genshin Checker/Model/Static/DiaryEventLocalize.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genshin_Checker.Model.Static.DiaryEventLocalize
+{
+    public class DiaryEventLocalize
+    {
+        public Dictionary<int, Event> Events { get; set; } = new();
+    }
+
+    public class Event
+    {
+        public Dictionary<string, Localize> Localize { get; set; } = new();
+        /// <summary>
+        /// カテゴリ
+        /// </summary>
+        public string Category { get; set; } = "";
+        /// <summary>
+        /// 再入手不可能であるか
+        /// </summary>
+        public bool IsClaimOnce { get; set; } = false;
+        /// <summary>
+        /// 期間制限があるか
+        /// </summary>
+        public bool IsLimited { get; set; } = false;
+
+    }
+    public class Localize
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string CategoryName { get; set; } = "";
+
+    }
+
+}
