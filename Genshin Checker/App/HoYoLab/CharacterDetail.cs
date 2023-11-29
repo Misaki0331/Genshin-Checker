b33d8715248c591c9d054b0ab0aff1042a708b35
水咲(みさき) <blueplanet65536@gmail.com>
2023-11-29 10:57:45 +0900

    天賦情報のレートリミット対策

--- Genshin Checker/App/HoYoLab/CharacterDetail.cs
+++ Genshin Checker/App/HoYoLab/CharacterDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace Genshin_Checker.App.HoYoLab
     {
         //キャッシュ時間
         const int CacheSecond = 3600;
-        public CharacterDetail(Account account) : base(account, 60000)
+        public CharacterDetail(Account account) : base(account, 1000)
         {
             ServerUpdate.Tick += Timeout_Tick;
             Cache = new();
@@ -21,9 +22,43 @@ namespace Genshin_Checker.App.HoYoLab
             public Model.HoYoLab.CharacterDetail.Data Data=new();
         }
         List<DataList> Cache;
-        private void Timeout_Tick(object? sender, EventArgs e)
+        private async void Timeout_Tick(object? sender, EventArgs e)
         {
-            foreach(var data in Cache.FindAll(A => A.ExpairTime < DateTime.UtcNow)) Cache.Remove(data);
+            Trace.WriteLine($"天賦レベル取得");
+            ServerUpdate.Stop();
+            ServerUpdate.Interval = 3600000;
+            try
+            {
+                var characters = await account.Characters.GetData();
+                foreach (var character in characters.avatars)
+                {
+                    for (int i = 0; i < 10; i++)
+                        try
+                        {
+                            var data = await GetData(character.id);
+                            Trace.WriteLine($"OK CharacterID:{character.id}");
+                            break;
+                        }catch (Account.HoYoLabAPIException){
+                            throw;
+                        }
+                        catch(Exception)
+                        {
+                            await Task.Delay(1000);
+                            if (i == 9) throw;
+                            continue;
+                        }
+                    await Task.Delay(1000);
+                }
+            }
+            catch (Account.HoYoLabAPIException ex)
+            {
+                Trace.WriteLine($"アカウント検証エラー : {ex.Retcode} - {ex.APIMessage}");
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.ToString());
+            }
+            ServerUpdate.Start();
         }
         public async Task<Model.HoYoLab.CharacterDetail.Data> GetData(int characterID)
         {
