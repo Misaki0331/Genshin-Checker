0b8a519fa961fc5a65b266971a46e5c4c86fd629
水咲(みさき) <blueplanet65536@gmail.com>
2023-12-11 22:03:43 +0900

    選択メッセージ追加

--- Genshin Checker/Window/PopupWindow/ChooseMessage.cs
+++ Genshin Checker/Window/PopupWindow/ChooseMessage.cs
@@ -2,20 +2,61 @@
 {
     public partial class ChooseMessage : Form
     {
-        public ChooseMessage(string title,string message,string windowtitle="エラーメッセージ")
+        public ChooseMessage(string title,string message,string windowtitle="情報",int selectcount=2,string select1="はい",string select2="いいえ",string select3="キャンセル",string select4="",string select5="")
         {
             InitializeComponent();
             Text = windowtitle;
             label1.Text = title;
             textBox1.Text = message.Replace("\r\n","\n").Replace("\n",Environment.NewLine);
-            pictureBox1.Image = resource.PaimonsPaint.Furina_2;
-
+            pictureBox1.Image = resource.PaimonsPaint.Furina_1;
+            button1.Text = select1;
+            button2.Text = select2;
+            button3.Text = select3;
+            button4.Text = select4;
+            button5.Text = select5;
+            var buttons = new List<Button>() { button1, button2, button3, button4, button5 };
+            for(int i=0;i<buttons.Count;i++)
+            {
+                if (selectcount > i) buttons[i].Visible = true;
+                else buttons[i].Visible = false;
+            }
             Icon = Icon.FromHandle(new Bitmap(pictureBox1.Image).GetHicon());
         }
-
+        public int Result = -1;
         private void button1_Click(object sender, EventArgs e)
         {
+            Result = 0;
             Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Result = 1;
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Result = 2;
+            Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Result = 3;
+            Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Result = 4;
+            Close();
+        }
+
+        private void ChooseMessage_Load(object sender, EventArgs e)
+        {
+            button1.Select();
         }
     }
 }
