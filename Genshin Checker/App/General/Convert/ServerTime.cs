360bd1bb3c70d0881683abd6eb791cf4d34d5e91
水咲(みさき) <blueplanet65536@gmail.com>
2023-12-11 18:23:23 +0900

    グループを追加

---
+++ Genshin Checker/App/General/Convert/ServerTime.cs
@@ -0,0 +1,58 @@
+﻿using Genshin_Checker.App.HoYoLab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genshin_Checker.App.General.Convert
+{
+    public static class ServerTime
+    {
+        private const int GameDailyResetTime = 4;
+        public static DateTime GameServerDate(Account.Servers server)
+        {
+            var time = GetRegionTime(server).AddHours(-GameDailyResetTime);
+            return new DateTime(time.Year, time.Month, time.Day);
+        } 
+        public static TimeSpan GameDailyReset(Account.Servers server)
+        {
+            var NextTime = GameServerDate(server).AddDays(1).AddHours(-GameDailyResetTime);
+            return NextTime - DateTime.UtcNow;
+        }
+        public static DateTime NextWeeklyResetDate(Account.Servers server)
+        {
+            int days = 0;
+            var date = GameServerDate(server).AddDays(1);
+            while (date.DayOfWeek != DayOfWeek.Monday)
+            {
+                date = date.AddDays(1);
+                days++;
+            }
+            return date.AddHours(-GameDailyResetTime);
+        }
+        public static TimeSpan GameWeeklyReset(Account.Servers server)
+        {
+            return NextWeeklyResetDate(server) - DateTime.UtcNow;
+        }
+        public static DateTime GetRegionTime(Account.Servers server)
+        {
+            return DateTime.UtcNow-ServerUTCOffset(server);
+        }
+        public static DateTime ConvertUTCTime(Account.Servers server, DateTime serverTime) {
+            return serverTime+ServerUTCOffset(server);
+
+        }
+        public static TimeSpan ServerUTCOffset(Account.Servers server)
+        {
+            return server switch
+            {
+                Account.Servers.os_usa => new TimeSpan(5, 0, 0),
+                Account.Servers.os_euro => new TimeSpan(-1, 0, 0),
+                Account.Servers.os_asia => new TimeSpan(-8, 0, 0),
+                Account.Servers.os_cht => new TimeSpan(-8, 0, 0),
+                _ => new TimeSpan(0, 0, 0)
+            };
+        }
+    }
+}
---
+++ Genshin Checker/App/General/Convert/World.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genshin_Checker.App.General.Convert
+{
+    public static class World
+    {
+        public enum Country
+        {
+            Mondstadt,
+            Liyue,
+            Inazuma,
+            Sumeru,
+            Fontaine,
+            Natlan,
+            Snezhnaya,
+            Khaenriah,
+            Celestia,
+            DarkSea,
+            Unknown,
+        }
+        public static Color GetBackgroundColor(Country country)
+        {
+            return country switch
+            {
+                Country.Mondstadt => Element.GetBackgroundColor(Element.ElementType.Anemo),
+                Country.Liyue => Element.GetBackgroundColor(Element.ElementType.Geo),
+                Country.Inazuma => Element.GetBackgroundColor(Element.ElementType.Electro),
+                Country.Sumeru => Element.GetBackgroundColor(Element.ElementType.Dendro),
+                Country.Fontaine => Element.GetBackgroundColor(Element.ElementType.Hydro),
+                Country.Natlan => Element.GetBackgroundColor(Element.ElementType.Pyro),
+                Country.Snezhnaya => Element.GetBackgroundColor(Element.ElementType.Cryo),
+                _ => Color.White
+            };
+        }
+
+    }
+}
