930af56e205475a651afa7c704026dc2c91b6d0d
Misaki <60120497+Misaki0331@users.noreply.github.com>
2024-04-14 12:07:19 +0900

    内部デバッグツールの機能強化

--- Genshin Checker/InternalTools/MusicInfoSetter.Designer.cs
+++ Genshin Checker/InternalTools/MusicInfoSetter.Designer.cs
@@ -29,21 +29,6 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            dataGridView1 = new DataGridView();
-            ID = new DataGridViewTextBoxColumn();
-            Time = new DataGridViewTextBoxColumn();
-            SetMode = new DataGridViewTextBoxColumn();
-            DataValue = new DataGridViewTextBoxColumn();
-            DeleteList = new Button();
-            NewList = new Button();
-            numericUpDown1 = new NumericUpDown();
-            numericUpDown2 = new NumericUpDown();
-            numericUpDown3 = new NumericUpDown();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            checkBox1 = new CheckBox();
-            comboBox1 = new ComboBox();
             SongPosition = new NumericUpDown();
             progressBar1 = new ProgressBar();
             ButtonPlay = new Button();
@@ -52,157 +37,11 @@
             label4 = new Label();
             timer2 = new System.Windows.Forms.Timer(components);
             label5 = new Label();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            textBox1 = new TextBox();
+            offset = new NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)SongPosition).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)offset).BeginInit();
             SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.AllowUserToAddRows = false;
-            dataGridView1.AllowUserToDeleteRows = false;
-            dataGridView1.AllowUserToResizeColumns = false;
-            dataGridView1.AllowUserToResizeRows = false;
-            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Sunken;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { ID, Time, SetMode, DataValue });
-            dataGridView1.Location = new Point(12, 12);
-            dataGridView1.MultiSelect = false;
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersVisible = false;
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dataGridView1.Size = new Size(254, 370);
-            dataGridView1.TabIndex = 0;
-            // 
-            // ID
-            // 
-            ID.HeaderText = "ID";
-            ID.Name = "ID";
-            ID.Visible = false;
-            // 
-            // Time
-            // 
-            Time.FillWeight = 70F;
-            Time.HeaderText = "時間";
-            Time.Name = "Time";
-            Time.ReadOnly = true;
-            // 
-            // SetMode
-            // 
-            SetMode.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
-            SetMode.FillWeight = 50F;
-            SetMode.HeaderText = "モード";
-            SetMode.Name = "SetMode";
-            SetMode.ReadOnly = true;
-            SetMode.Width = 80;
-            // 
-            // DataValue
-            // 
-            DataValue.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
-            DataValue.FillWeight = 50F;
-            DataValue.HeaderText = "値";
-            DataValue.Name = "DataValue";
-            DataValue.ReadOnly = true;
-            DataValue.Width = 80;
-            // 
-            // DeleteList
-            // 
-            DeleteList.Location = new Point(272, 359);
-            DeleteList.Name = "DeleteList";
-            DeleteList.Size = new Size(57, 23);
-            DeleteList.TabIndex = 1;
-            DeleteList.Text = "削除";
-            DeleteList.UseVisualStyleBackColor = true;
-            // 
-            // NewList
-            // 
-            NewList.Location = new Point(272, 330);
-            NewList.Name = "NewList";
-            NewList.Size = new Size(57, 23);
-            NewList.TabIndex = 2;
-            NewList.Text = "新規";
-            NewList.UseVisualStyleBackColor = true;
-            // 
-            // numericUpDown1
-            // 
-            numericUpDown1.Location = new Point(372, 330);
-            numericUpDown1.Maximum = new decimal(new int[] { 10000000, 0, 0, 0 });
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(70, 23);
-            numericUpDown1.TabIndex = 3;
-            numericUpDown1.TextAlign = HorizontalAlignment.Right;
-            // 
-            // numericUpDown2
-            // 
-            numericUpDown2.DecimalPlaces = 2;
-            numericUpDown2.Location = new Point(405, 359);
-            numericUpDown2.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
-            numericUpDown2.Name = "numericUpDown2";
-            numericUpDown2.Size = new Size(70, 23);
-            numericUpDown2.TabIndex = 4;
-            numericUpDown2.TextAlign = HorizontalAlignment.Right;
-            numericUpDown2.Value = new decimal(new int[] { 120, 0, 0, 0 });
-            // 
-            // numericUpDown3
-            // 
-            numericUpDown3.Location = new Point(335, 359);
-            numericUpDown3.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
-            numericUpDown3.Name = "numericUpDown3";
-            numericUpDown3.Size = new Size(40, 23);
-            numericUpDown3.TabIndex = 5;
-            numericUpDown3.TextAlign = HorizontalAlignment.Right;
-            numericUpDown3.Value = new decimal(new int[] { 4, 0, 0, 0 });
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(381, 363);
-            label1.Name = "label1";
-            label1.Size = new Size(18, 15);
-            label1.TabIndex = 6;
-            label1.Text = "/4";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(481, 363);
-            label2.Name = "label2";
-            label2.Size = new Size(32, 15);
-            label2.TabIndex = 7;
-            label2.Text = "BPM";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(335, 334);
-            label3.Name = "label3";
-            label3.Size = new Size(31, 15);
-            label3.TabIndex = 8;
-            label3.Text = "時間";
-            // 
-            // checkBox1
-            // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(519, 362);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(56, 19);
-            checkBox1.TabIndex = 9;
-            checkBox1.Text = "GoGo";
-            checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "拍子", "BPM", "サビモード" });
-            comboBox1.Location = new Point(448, 329);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 23);
-            comboBox1.TabIndex = 10;
             // 
             // SongPosition
             // 
@@ -269,56 +108,49 @@
             label5.TabIndex = 16;
             label5.Text = "label5";
             // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            textBox1.Location = new Point(12, 6);
+            textBox1.MaxLength = 9999999;
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Horizontal;
+            textBox1.Size = new Size(258, 376);
+            textBox1.TabIndex = 17;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // offset
+            // 
+            offset.Location = new Point(294, 124);
+            offset.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            offset.Minimum = new decimal(new int[] { 10000, 0, 0, int.MinValue });
+            offset.Name = "offset";
+            offset.Size = new Size(120, 23);
+            offset.TabIndex = 18;
+            // 
             // MusicInfoSetter
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(670, 394);
+            Controls.Add(offset);
+            Controls.Add(textBox1);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(ButtonStop);
             Controls.Add(ButtonPlay);
             Controls.Add(progressBar1);
             Controls.Add(SongPosition);
-            Controls.Add(comboBox1);
-            Controls.Add(checkBox1);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(numericUpDown3);
-            Controls.Add(numericUpDown2);
-            Controls.Add(numericUpDown1);
-            Controls.Add(NewList);
-            Controls.Add(DeleteList);
-            Controls.Add(dataGridView1);
             Name = "MusicInfoSetter";
             Text = "MusicInfoSetter";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
             ((System.ComponentModel.ISupportInitialize)SongPosition).EndInit();
+            ((System.ComponentModel.ISupportInitialize)offset).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private DataGridView dataGridView1;
-        private Button DeleteList;
-        private Button NewList;
-        private NumericUpDown numericUpDown1;
-        private NumericUpDown numericUpDown2;
-        private NumericUpDown numericUpDown3;
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private CheckBox checkBox1;
-        private DataGridViewTextBoxColumn ID;
-        private DataGridViewTextBoxColumn Time;
-        private DataGridViewTextBoxColumn SetMode;
-        private DataGridViewTextBoxColumn DataValue;
-        private ComboBox comboBox1;
         private NumericUpDown SongPosition;
         private ProgressBar progressBar1;
         private Button ButtonPlay;
@@ -327,5 +159,7 @@
         private Label label4;
         private System.Windows.Forms.Timer timer2;
         private Label label5;
+        private TextBox textBox1;
+        private NumericUpDown offset;
     }
 }
--- Genshin Checker/InternalTools/MusicInfoSetter.cs
+++ Genshin Checker/InternalTools/MusicInfoSetter.cs
@@ -15,6 +15,88 @@ namespace Genshin_Checker.InternalTools
 {
     public partial class MusicInfoSetter : Form
     {
+        public class MusicEvent
+        {
+            public string type = "";
+            public double beat;
+            public double value;
+        }
+        public BeatTimeConverter beat = new(new());
+        public class BeatTimeConverter
+        {
+            class MusicDetailEvent : MusicEvent
+            {
+                public TimeSpan TriggerStart;
+            };
+            private List<MusicDetailEvent> events;
+            private List<MusicEvent> bpm;
+
+            public BeatTimeConverter(List<MusicEvent> bpmEvents)
+            {
+                // BPMイベントだけをフィルタリングして、拍子に沿ってソートする
+                bpm = bpmEvents.Where(e => e.type == "BPM").OrderBy(e => e.beat).ToList();
+                events = new();
+                foreach (var a in bpmEvents) events.Add(new() { type = a.type, beat = a.beat, value = a.value, TriggerStart = TimeSpan.FromSeconds(BeatsToTime(a.beat)) });
+            }
+            public class Result
+            {
+                public double second;
+                public double bpm;
+            }
+            // 時間からビート数を計算する
+            public Result TimeToBeats(double timeInSeconds)
+            {
+                lock (events)
+                {
+                    double currentBeat = 0.0;
+                    double currentTime = 0.0;
+                    double lastBPM = 120.0; // 初期BPMを120とする（指定がない場合のデフォルト）
+
+                    foreach (var evt in events)
+                    {
+                        // 次のBPMイベントまでの時間を計算
+                        double nextTime = currentTime + (60.0 / lastBPM) * (evt.beat - currentBeat);
+                        if (timeInSeconds < nextTime)
+                        {
+                            // 指定された時間がこのイベントの前にある場合、中断して計算
+                            return new() { bpm = lastBPM, second = currentBeat + (timeInSeconds - currentTime) * lastBPM / 60.0 };
+                        }
+
+                        // 更新
+                        currentBeat = evt.beat;
+                        currentTime = nextTime;
+                        lastBPM = evt.value;
+                    }
+                    // 最後のBPMイベントから時間の終わりまで
+                    return new() { bpm = lastBPM, second = currentBeat + (timeInSeconds - currentTime) * lastBPM / 60.0 };
+                }
+            }
+
+            // ビート数から時間を計算する
+            public double BeatsToTime(double beats)
+            {
+                double currentBeat = 0.0;
+                double currentTime = 0.0;
+                double lastBPM = 120.0; // 初期BPMを120とする（指定がない場合のデフォルト）
+
+                foreach (var evt in events)
+                {
+                    if (beats < evt.beat)
+                    {
+                        // 指定されたビート数がこのイベントの前にある場合、中断して計算
+                        return currentTime + (60.0 / lastBPM) * (beats - currentBeat);
+                    }
+
+                    // 次のBPMイベントまでの時間を更新
+                    currentTime += (60.0 / lastBPM) * (evt.beat - currentBeat);
+                    currentBeat = evt.beat;
+                    lastBPM = evt.value;
+                }
+
+                // 最後のBPMイベントからビートの終わりまで
+                return currentTime + (60.0 / lastBPM) * (beats - currentBeat);
+            }
+        }
         MemoryStream? MusicStream;
         WaveStream? WaveStream;
         WaveOut waveOut;
@@ -23,7 +105,7 @@ namespace Genshin_Checker.InternalTools
         public MusicInfoSetter()
         {
             InitializeComponent();
-            waveOut = new WaveOut();
+            waveOut = new WaveOut() {DesiredLatency = 10, NumberOfBuffers =500 };
             waveOut.PlaybackStopped += WaveOut_PlaybackStopped;
             stopwatch = new();
         }
@@ -50,12 +132,8 @@ namespace Genshin_Checker.InternalTools
         }
         public async Task FileInit(string url)
         {
-            var data = await App.WebRequest.GetRequest(url);
-            if (data == null) return;
-            Trace.WriteLine($"Size:{data.Length / 1024.0 / 1024.0:0.00}MB");
-            MusicStream = new(data);
             if (WaveStream != null && WaveStream.CanRead) await WaveStream.DisposeAsync();
-            WaveStream = new Mp3FileReader(MusicStream);
+            WaveStream = new Mp3FileReader(url);
             waveOut.Init(WaveStream);
             SongPosition.Maximum = (int)WaveStream.TotalTime.TotalMilliseconds;
             progressBar1.Maximum = (int)WaveStream.Length;
@@ -73,14 +151,11 @@ namespace Genshin_Checker.InternalTools
             {
                 if (progressBar1.Maximum < (int)WaveStream.Position) progressBar1.Value = progressBar1.Maximum;
                 else progressBar1.Value = (int)WaveStream.Position;
-                var cr = CurrentTime + stopwatch.Elapsed;
+                var cr = waveOut.PlaybackState==PlaybackState.Playing?WaveStream.CurrentTime-System.TimeSpan.FromSeconds(0.5):WaveStream.CurrentTime;
                 if (SongPosition.Maximum < (int)cr.TotalMilliseconds) SongPosition.Value = SongPosition.Maximum;
-                else SongPosition.Value = (int)cr.TotalMilliseconds;
                 label4.Text = $"{WaveStream.CurrentTime:m\\:ss\\:fff}({cr:m\\:ss\\:fff}) / {WaveStream.TotalTime:m\\:ss\\:fff}";
-                var beat = (cr.TotalMilliseconds - (double)numericUpDown1.Value) / (60000.0 / (double)numericUpDown2.Value);
-                int measure = (int)(beat / (double)numericUpDown3.Value);
-                double left = beat-measure*(int)numericUpDown3.Value+1;
-                label5.Text = $"{measure} {left:0.000}";
+                var res = beat.TimeToBeats(cr.TotalSeconds - (double)offset.Value / 1000.0);
+                label5.Text = $"{res.second:0.000}  {res.bpm} BPM";
             }
         }
 
@@ -88,11 +163,11 @@ namespace Genshin_Checker.InternalTools
         {
             if (SongPosition.Enabled) if (WaveStream != null)
                 {
-                    WaveStream.CurrentTime = new((int)SongPosition.Value * 10000);
+                    WaveStream.CurrentTime = TimeSpan.FromSeconds((double)SongPosition.Value / 1000.0);
                     stopwatch.Stop();
                     stopwatch.Reset();
                     if (waveOut.PlaybackState == PlaybackState.Playing) stopwatch.Start();
-                    CurrentTime = new((int)SongPosition.Value * 10000);
+                    CurrentTime = TimeSpan.FromSeconds((double)SongPosition.Value / 1000.0);
                 }
         }
 
@@ -144,6 +219,26 @@ namespace Genshin_Checker.InternalTools
             timer2.Stop();
             timer2.Interval = 100;
             SongPosition.Enabled = true;
+        }
+        List<MusicEvent> timelist=new();
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            lock (timelist)
+            {
+                timelist.Clear();
+                foreach (var text in textBox1.Lines)
+                {
+                    var spl = text.Split(",");
+                    if (spl.Length == 3)
+                    {
+                        if (double.TryParse(spl[0], out var measure) && double.TryParse(spl[2],out var value))
+                        {
+                            timelist.Add(new MusicEvent() { beat = measure, type = spl[1], value = value == 0 ? 0.000001 : value });
+                        }
+                    }
+                }
+                beat = new(timelist);
+            }
         }
     }
 }
