46ba59219a7f520a80dbda941630bef5f98a4764
Misaki <60120497+Misaki0331@users.noreply.github.com>
2025-01-20 14:25:04 +0900

    プレビュー画像の追加

--- Genshin Checker/GUI/Window/HoYoContentViewer.xaml.cs
+++ Genshin Checker/GUI/Window/HoYoContentViewer.xaml.cs
@@ -21,6 +21,36 @@ namespace Genshin_Checker.GUI.Window
         private async void InitializeWebView2()
         {
             await HtmlViewer.EnsureCoreWebView2Async(null);
+            HtmlViewer.CoreWebView2InitializationCompleted += HtmlViewer_CoreWebView2InitializationCompleted;
+        }
+
+        private void HtmlViewer_CoreWebView2InitializationCompleted(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            HtmlViewer.CoreWebView2.NavigationStarting += (sender, args) =>
+            {
+                if (args.IsUserInitiated)
+                {
+                    args.Cancel = true;
+                }
+            };
+
+            HtmlViewer.CoreWebView2.HistoryChanged += (sender, args) =>
+            {
+                if (HtmlViewer.CoreWebView2.CanGoBack)
+                {
+                    HtmlViewer.CoreWebView2.GoBack();
+                }
+            };
+
+            HtmlViewer.CoreWebView2.ContextMenuRequested += (sender, args) =>
+            {
+                args.Handled = true;
+            };
+
+            if (ArticleList.Items.Count > 0) ArticleList.SelectedIndex = 0;
+#if !DEBUG
+            HtmlViewer.CoreWebView2.Settings.AreDevToolsEnabled = false;
+#endif
         }
 
         private async void LoadArticles()
@@ -29,7 +59,7 @@ namespace Genshin_Checker.GUI.Window
 
             var articles = await FetchArticlesAsync();
             ArticleList.ItemsSource = articles;
-            if(ArticleList.Items.Count>0) ArticleList.SelectedIndex = 0;
+            if (ArticleList.Items.Count > 0) ArticleList.SelectedIndex = 0;
         }
 
         private async Task<List<Article>> FetchArticlesAsync()
@@ -58,13 +88,15 @@ namespace Genshin_Checker.GUI.Window
                             articles.Add(new Article
                             {
                                 Title = result.Subject,
-                                Summary = result.Description,
-                                Content = result.Content.HTMLText + AddContent
+                                Summary = result.Content.IsSimple ? result.Content.HTMLText.Replace("<br/>", "\n") : result.Description,
+                                Content = result.Content.HTMLText + AddContent,
+                                PreviewImage = result.Content.Images.Count > 0 ? result.Content.Images[0] : ""
                             });
                         }
                     }
                 }
-            }catch(Exception ex)
+            }
+            catch (Exception ex)
             {
                 Log.Error(ex);
                 Dialog.Error("取得エラー", $"{ex}");
@@ -75,12 +107,32 @@ namespace Genshin_Checker.GUI.Window
 
         private void ArticleList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (ArticleList.SelectedItem is Article selectedArticle)
+            try
             {
-                HtmlViewer.NavigateToString("<meta charset=\"UTF-8\"><style>img { width: 100%; height: auto; }\niframe { width: 100%; aspect-ratio: 16 / 9;}</style>" + selectedArticle.Content);
-                Title = selectedArticle.Title;
+                if (ArticleList.SelectedItem is Article selectedArticle)
+                {
+                    HtmlViewer.NavigateToString("<meta charset=\"UTF-8\"><style>img { width: 100%; height: auto; }\niframe { width: 100%; aspect-ratio: 16 / 9;}</style>" + selectedArticle.Content);
+                    Title = selectedArticle.Title;
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                HtmlViewer.CoreWebView2InitializationCompleted += (s, e) =>
+                {
+                    if (ArticleList.SelectedItem is Article selectedArticle)
+                    {
+                        HtmlViewer.NavigateToString("<meta charset=\"UTF-8\"><style>img { width: 100%; height: auto; }\niframe { width: 100%; aspect-ratio: 16 / 9;}</style>" + selectedArticle.Content);
+                        Title = selectedArticle.Title;
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+
+                Log.Error(ex);
             }
         }
+
     }
 
     public class Article
@@ -88,5 +140,6 @@ namespace Genshin_Checker.GUI.Window
         public string Title { get; set; } = "";
         public string Summary { get; set; } = "";
         public string Content { get; set; } = "";
+        public string PreviewImage { get; set; } = "";
     }
 }
